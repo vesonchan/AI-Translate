9d0d7382f09d3d05984de5f213ea44ba520b686d
qyzhg <qyzhg@qyzhg.com>
2025-11-26 11:03:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [vue()],
 
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        "area-selector": "area-selector.html",
+      },
+    },
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
@@ -18,10 +26,10 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
